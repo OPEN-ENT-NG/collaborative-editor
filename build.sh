6c360fef08da78c8b2bdbb9117af5dcc76314a64
dboissin <damien.boissin@web-education.net>
2018-02-22 18:30:19 +0100

    Improve build.sh

--- build.sh
+++ build.sh
@@ -11,26 +11,52 @@ then
   export GROUP_GID=1000
 fi
 
-if [ "$1" == "clean" ]
-then
+clean () {
   docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle clean
-fi
-if [ $? -eq 0 ]
-then
-  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm install && node_modules/gulp/bin/gulp.js build" && docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle shadowJar install publishToMavenLocal
-fi
-if [ $? -eq 0 ]
-then
-  if [ "$2" == "publish" ]
+}
+
+buildNode () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "npm install && node_modules/gulp/bin/gulp.js build"
+}
+
+buildGradle () {
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle shadowJar install publishToMavenLocal
+}
+
+publish () {
+  if [ -e "?/.gradle" ] && [ ! -e "?/.gradle/gradle.properties" ]
   then
-    if [ -e "?/.gradle" ] && [ ! -e "?/.gradle/gradle.properties" ]
-    then
-      echo "odeUsername=$NEXUS_ODE_USERNAME" > "?/.gradle/gradle.properties"
-      echo "odePassword=$NEXUS_ODE_PASSWORD" >> "?/.gradle/gradle.properties"
-      echo "sonatypeUsername=$NEXUS_SONATYPE_USERNAME" >> "?/.gradle/gradle.properties"
-      echo "sonatypePassword=$NEXUS_SONATYPE_PASSWORD" >> "?/.gradle/gradle.properties"
-    fi
-    docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle publish
+    echo "odeUsername=$NEXUS_ODE_USERNAME" > "?/.gradle/gradle.properties"
+    echo "odePassword=$NEXUS_ODE_PASSWORD" >> "?/.gradle/gradle.properties"
+    echo "sonatypeUsername=$NEXUS_SONATYPE_USERNAME" >> "?/.gradle/gradle.properties"
+    echo "sonatypePassword=$NEXUS_SONATYPE_PASSWORD" >> "?/.gradle/gradle.properties"
   fi
-fi
+  docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle publish
+}
+
+for param in "$@"
+do
+  case $param in
+    clean)
+      clean
+      ;;
+    buildNode)
+      buildNode
+      ;;
+    buildGradle)
+      buildGradle
+      ;;
+    install)
+      buildGradle # buildNode && buildGradle
+      ;;
+    publish)
+      publish
+      ;;
+    *)
+      echo "Invalid argument : $param"
+  esac
+  if [ ! $? -eq 0 ]; then
+    exit 1
+  fi
+done
 
